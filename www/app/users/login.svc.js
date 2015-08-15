238d9a12eb7f367ceccde8ccb4b3dacf42c03633
fgolemo <fgolemo@gmail.com>
2015-08-15 20:57:51 +0200

    added basic login page on profile

---
+++ www/app/users/login.svc.js
@@ -0,0 +1,35 @@
+usersModule.service('Login', ['$http', 'User', 'Settings', function ($http, User, Settings) {
+    this.user = null;
+    this.isLoggedIn = function() {
+        return !!this.user;
+    };
+    this.sendLogin = function(data) {
+        $http.post(Settings.database + "login", data)
+            .then(function (response) { // when response is available
+                console.log("got positive login response:");
+                console.dir(response);
+            }, function (response) { // when there was an error
+                console.log("couldn't log in with data:");
+                console.dir(data);
+                console.log("server response:")
+                console.dir(response);
+            }
+        );
+    };
+
+    this.sendSignup = function(data) {
+        $http.post(Settings.database + "signup", data)
+            .then(function (response) { // when response is available
+                console.log("got positive signup response:");
+                console.dir(response);
+            }, function (response) { // when there was an error
+                console.log("couldn't sign up with data:");
+                console.dir(data);
+                console.log("server response:")
+                console.dir(response);
+            }
+        );
+    };
+
+    //TODO: handle signup
+}]);
--- www/app/users/user.info.ctrl.js
+++ www/app/users/user.info.ctrl.js
@@ -1,7 +1,23 @@
 usersModule
-    .controller('UserInfoCtrl', function ($scope, UsersManager) {
-        UsersManager.m.get(1).then(function (user) {
-            $scope.anon = user;
-        });
+    .controller('UserInfoCtrl', function ($scope, UsersManager, Login) {
+        //UsersManager.m.get(0).then(function (user) {
+        //    $scope.anon = user;
+        //});
+        //TODO: load previous user credentials from localstorage
+        $scope.login = Login;
+        $scope.loginData = {};
+        var SHA256 =  new Hashes.SHA256;
+        var getData = function () {
+            return {
+                name: $scope.loginData.username,
+                hash: SHA256.hex($scope.loginData.password)
+            };
+        };
+        $scope.sendLogin = function() {
+            Login.sendLogin(getData());
+        };
+        $scope.sendSignup = function() {
+            Login.sendSignup(getData());
+        };
     })
 ;
