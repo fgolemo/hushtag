b359a47690b39f939cb2a8d43aeaf91c2d07de60
fgolemo <fgolemo@gmail.com>
2015-09-08 20:13:10 +0200

    made location-select into modal, but need to convert object to bind to from event to general obj

--- www/app/events/event.create.ctrl.js
+++ www/app/events/event.create.ctrl.js
@@ -7,25 +7,5 @@ eventsModule
                 Helper.updateCallback("event", response);
             });
         };
-        LocationsManager.m.loadAll().then(function(locations) {
-            $scope.locations = locations;
-        });
-        var locationSelect = new Modal(
-            $scope,
-            'app/locations/locationselect.modal.html',
-            angular.noop,
-            function (location) {
-                if (location == null) {
-                    return; // if user hits "cancel", i.e. don't change the current location
-                }
-                if (location == "none") {
-                    delete $scope.event.locationResolved;
-                    $scope.event.location = "";
-                } else {
-                    $scope.event.locationResolved = location;
-                    $scope.event.location = location.id;
-                }
-            });
-        locationSelect.init();
     })
 ;
--- www/app/locations/locationselect.dir.js
+++ www/app/locations/locationselect.dir.js
@@ -1,48 +1,30 @@
-locationsModule.directive('locationSelect', ['$ionicPopup', 'Login',
-    function ($ionicPopup, Login) {
+locationsModule.directive('locationSelect', ['Modal', 'LocationsManager',
+    function (Modal, LocationsManager) {
         return {
             restrict: 'E',
             replace: 'true',
-            templateUrl: 'app/votes/votes.dir.html',
+            templateUrl: 'app/locations/locationselect.dir.html',
             link: function (scope, elem, attrs) {
-                scope.login = Login;
-                scope.$on("objLoaded", function () {
-                    scope.obj = scope[attrs.on];
-                    var hasVotedChecker = Votes.hasVoted(attrs.on, scope.obj.id);
-                    if (hasVotedChecker != null) {
-                        hasVotedChecker.then(function (response) {
-                            var r = response.data;
-                            scope.obj.hasVoted = !!(r.status && r.status == "success" && r.reply);
-                        });
-                    }
+                LocationsManager.m.loadAll().then(function(locations) {
+                    scope.locations = locations;
                 });
-                scope.$emit("voterReady");
-                var vote = function(updown) {
-                    var operation = (updown=="up"?"up":"down")+"vote";
-                    Votes[operation](attrs.on, scope.obj.id).then(function (response) {
-                        if (response.data && response.data.status && response.data.status == "success") {
-                            scope.doRefresh();
-                            $ionicPlatform.ready(function () {
-                                try {
-                                    $cordovaToast.show(updown+"voted", "short", "center");
-                                } catch (err) {
-                                    console.log("ERROR: cordova not working");
-                                }
-                            });
-                        } else if (response.data && response.data.status && response.data.status == "fail") {
-                            $ionicPopup.alert({
-                                title: "Oh noes.",
-                                template: msg
-                            });
+                var locationSelect = new Modal(
+                    scope,
+                    'app/locations/locationselect.modal.html',
+                    angular.noop,
+                    function (location) {
+                        if (location == null) {
+                            return; // if user hits "cancel", i.e. don't change the current location
+                        }
+                        if (location == "none") {
+                            delete scope.event.locationResolved;
+                            scope.event.location = "";
+                        } else {
+                            scope.event.locationResolved = location;
+                            scope.event.location = location.id;
                         }
                     });
-                };
-                scope.upvote = function () {
-                    vote("up");
-                };
-                scope.downvote = function () {
-                    vote("down");
-                };
+                locationSelect.init();
             }
         };
     }]);
