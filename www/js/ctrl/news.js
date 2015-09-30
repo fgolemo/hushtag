5a0a5f4fb46282e3138843763ab185f0ddcf4404
fgolemo <fgolemo@gmail.com>
2015-10-01 00:52:17 +0200

    cleaned up news page

--- www/js/ctrl/news.js
+++ www/js/ctrl/news.js
@@ -1,9 +1,9 @@
 controllerModule
-    .controller('NewsCtrl', function($scope, Modal, TestService) {
+    .controller('NewsCtrl', function ($scope, Modal, TestService) {
 
-        var m = new Modal($scope, "contact-modal.html", angular.noop, angular.noop);
-        m.init();
-        $scope.ts = new TestService("test");
+        //var m = new Modal($scope, "contact-modal.html", angular.noop, angular.noop);
+        //m.init();
+        //$scope.ts = new TestService("test");
 
     })
 ;
