a82dc446dea75d77a26e96d55fc1d642b5c108d2
fgolemo <fgolemo@gmail.com>
2015-10-29 09:45:54 +0100

    TMP: added random genres

--- www/app/events/events.ctrl.js
+++ www/app/events/events.ctrl.js
@@ -19,6 +19,20 @@ eventsModule
                 loadData(true, function() {
                     $scope.$broadcast('scroll.refreshComplete');
                 });
+            };
+            $scope.randomGenre = function(eventID) {
+                var genres = [
+                    "house",
+                    "house",
+                    "salsa",
+                    "electronica",
+                    "dance"
+                ];
+                if (eventID-1 < genres.length) {
+                    return genres[eventID-1];
+                } else {
+                    return genres[Math.floor(Math.random()*genres.length)];
+                }
             }
         }
     ]
