677a0be8c947e933e5c76c9fd20e784d841f8820
fgolemo <fgolemo@gmail.com>
2016-01-16 18:57:32 +0100

    tagging works - may need some better positioning though

--- www/app/comments/commentcreate.dir.js
+++ www/app/comments/commentcreate.dir.js
@@ -1,17 +1,20 @@
-commentsModule.directive('commentCreate', ['$ionicPopup', 'Login', 'Comment', '$timeout', 'CommentsManager',
-    function ($ionicPopup, Login, Comment, $timeout, CommentsManager) {
+commentsModule.directive('commentCreate', ['$ionicPopup', 'Login', 'Comment', '$timeout', 'CommentsManager', 'Tags', '$ionicPopover', '$window',
+    function ($ionicPopup, Login, Comment, $timeout, CommentsManager, Tags, $ionicPopover, $window) {
+        Tags.get(true);
         return {
             restrict: 'E',
             replace: 'true',
             templateUrl: 'app/comments/commentcreate.dir.html',
             link: function (scope, elem, attrs) {
+                scope.tags = Tags;
                 scope.showCommentBox = false;
                 scope.loadCommentBox = function () {
                     scope.showCommentBox = true;
                     scope.newComment = new Comment();
                     $timeout(function () {
-                        document.getElementById("comment-create-box").focus();
+                        $window.document.getElementById("comment-create-box").focus();
                     }, 750);
+
                 };
                 scope.sendComment = function () {
                     scope.newComment.owner = Login.user.id;
@@ -20,11 +23,46 @@ commentsModule.directive('commentCreate', ['$ionicPopup', 'Login', 'Comment', '$
                             scope.newComment = new Comment();
                             scope.showCommentBox = false;
                         }
-                    }, function(err) {
+                    }, function (err) {
                         console.log("ERR: there was a problem adding the comment:");
                         console.dir(err);
                     });
-                }
+                };
+                // .fromTemplateUrl() method
+                $ionicPopover.fromTemplateUrl('/app/tags/tagselect.popover.html', {
+                    scope: scope
+                }).then(function (popover) {
+                    scope.popover = popover;
+                });
+
+                scope.poHandler = {
+                    openPopover: function ($event, matches) {
+                        scope.popover.show($event);
+                        scope.popover.matches = matches;
+                    },
+                    closePopover: function (selectedMatch) {
+                        if (selectedMatch && selectedMatch.name && selectedMatch.name.length > 0) {
+                            scope.newComment.content = Tags.insertTag(scope.newComment.content, selectedMatch);
+                            scope.popover.hide();
+                            $timeout(function () {
+                                $window.document.getElementById("comment-create-box").focus();
+                            }, 800);
+                        }
+                    }
+                };
+                //Cleanup the popover when we're done with it!
+                scope.$on('$destroy', function () {
+                    scope.popover.remove();
+                });
+                // Execute action on hide popover
+                scope.$on('popover.hidden', function () {
+                    // Execute action
+                });
+                // Execute action on remove popover
+                scope.$on('popover.removed', function () {
+                    // Execute action
+                });
+
             }
         };
     }]);
--- www/app/tags/tags.svc.js
+++ www/app/tags/tags.svc.js
@@ -2,6 +2,8 @@ tagsModule.service('Tags', ['Settings', '$q', '$http', function (Settings, $q, $
 
     this.tags = [];
 
+    this._minimumSearchStringLength = 3;
+
     this.get = function (force) {
         var scope = this;
 
@@ -29,6 +31,57 @@ tagsModule.service('Tags', ['Settings', '$q', '$http', function (Settings, $q, $
                 });
         }
         return deferred.promise;
+    };
+
+    this.getAllStrings = function() {
+        var out = [];
+        for(var i in tags) {
+            out.push(tags[i].name);
+        }
+        return out;
+    };
+
+    this._getTag = function(text) {
+        var pieces = text.split(" ");
+        var hashpieces = pieces[pieces.length-1].split("#");
+
+        if(hashpieces.length == 1 || hashpieces[hashpieces.length-1].length < this._minimumSearchStringLength) {
+            return false;
+        } else {
+            return hashpieces[hashpieces.length-1];
+        }
+    };
+
+    this._findInTags = function(tag) {
+        var matches = [];
+        for(var i in this.tags) {
+            if (this.tags[i].name.toLowerCase().indexOf(tag.toLowerCase()) > -1 ||
+                this.tags[i].type.indexOf(tag.toLowerCase()) > -1) {
+                matches.push(this.tags[i]);
+            }
+        }
+        return matches;
+    };
+
+    this.detect = function(textInput, poHandler, $event) {
+        var tag = this._getTag(textInput);
+        if (tag !== false) {
+            var matches = this._findInTags(tag);
+            if (matches.length > 0) {
+                poHandler.openPopover($event, matches);
+            } else {
+                poHandler.closePopover(false);
+            }
+        } else {
+            poHandler.closePopover(false);
+        }
+    };
+
+    this.insertTag = function(textModel, match) {
+        var pieces = textModel.split("#");
+        pieces[pieces.length-1] = match.slug + " ";
+        var out = pieces.join("#");
+        return out;
     }
 
 }]);
